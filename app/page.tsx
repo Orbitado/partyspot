15dcf388ac2b9074e47f82c6c15ce1d9daea7add
Orbitado <juanpaavaldezz@gmail.com>
2025-03-21 11:13:22 -0300

    feat: Add Featured Spots Section and Update Home Page Layout
    - Introduced a new `FeaturedSpotsSection` component to display highlighted event spaces.
    - Updated `page.tsx` to include the `FeaturedSpotsSection` alongside the existing `Navbar` and `HeroSection`.
    - Created `FeaturedSpotsCard` component for individual spot representation with details such as title, price, rating, and location.
    - Added a new data file `spots.ts` to manage spot information and integrated it into the `FeaturedSpots` component.
    - Enhanced `BackgroundPageContainer` styles for improved layout consistency.

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,14 @@
-import HeroSection from "@/src/components/hero-section";
-import Navbar from "@/src/components/navbar/navbar";
 import type { NextPage } from "next";
+import Navbar from "@/src/components/navbar/navbar";
+import HeroSection from "@/src/components/hero-section";
+import FeaturedSpotsSection from "@/src/components/featured-spots-section/featured-spots";
 
 const Home: NextPage = () => {
   return (
     <main id="main-content">
       <Navbar />
       <HeroSection />
+      <FeaturedSpotsSection />
     </main>
   );
 };
--- src/components/common/background-page-container.tsx
+++ src/components/common/background-page-container.tsx
@@ -12,7 +12,7 @@ export default function BackgroundPageContainer({
 
   return (
     <div
-      className="animate-fadeIn container mx-auto grid min-h-screen grid-rows-[auto_1fr_auto] items-center justify-center gap-4 text-white before:absolute before:inset-0 before:-z-50 before:bg-black before:opacity-90 before:transition-all before:duration-700 before:ease-in-out after:absolute after:inset-0 after:-z-50 after:bg-cover after:bg-center after:opacity-40 after:transition-all after:duration-700 after:ease-in-out"
+      className="animate-fadeIn container mx-auto mb-12 grid min-h-screen grid-rows-[auto_1fr_auto] items-center justify-center gap-4 text-white before:absolute before:inset-0 before:-z-50 before:bg-black before:opacity-90 before:transition-all before:duration-700 before:ease-in-out after:absolute after:inset-0 after:-z-50 after:bg-cover after:bg-center after:opacity-40 after:transition-all after:duration-700 after:ease-in-out sm:mb-16 md:mb-20"
       style={
         {
           "--bg-url": `url(${backgroundUrl})`
