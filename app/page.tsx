de0c3dbfa87b71b4d96b2a5fcbfddb3aaf923e3c
Orbitado <juanpaavaldezz@gmail.com>
2025-03-27 15:26:15 -0300

    feat: Add Navbar and Footer components to layout
    - Introduced a new `Navbar` component for improved navigation, including dynamic link handling and user actions.
    - Created a `Footer` component to enhance the footer section with social links and company information.
    - Updated `layout.tsx` to integrate both `Navbar` and `Footer`, ensuring a cohesive layout structure.
    - Removed the previous Navbar import from `page.tsx` to streamline the component usage.

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,4 @@
 import type { NextPage } from "next";
-import Navbar from "@/src/components/navbar/navbar";
 import HeroSection from "@/src/components/hero-section/hero-section";
 import FeaturedSpotsSection from "@/src/components/featured-spots-section/featured-spots";
 import MadeWithPartySpotSection from "@/src/components/made-with-ps-section/made-with-party-spot-section";
@@ -9,7 +8,6 @@ import NextParty from "@/src/components/cta-next-party/cta-next-party";
 const Home: NextPage = () => {
   return (
     <main id="main-content">
-      <Navbar />
       <HeroSection />
       <FeaturedSpotsSection />
       <NextParty />
